490c3b7439b48211700758ccb2e5489c5b918eb0
omyman12 <qkrrlcks00@gmail.com>
2024-10-21 12:43:26 +0900

    Initial commit

---
+++ Assets/Scripts/ScoreButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+//델리게이트 = 레시피다
+// 워크를 실행하면 좌표변경, 사운드, 에니메이션을 재생하는걸 워크에 묶어서 ㅇ넣어
+//트리거를 발동해서 인보크하면 같이실행되게 도와준다
