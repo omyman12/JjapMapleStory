992980793bf8487e4e6b74f7269f92654e5f5796
omyman12 <qkrrlcks00@gmail.com>
2024-10-21 14:02:33 +0900

    update
    update

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -13,10 +13,16 @@ public class AnimationController : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
+    void SetanimationVariable()
+    {
+        animator.SetInteger("Mushroom_Idle", animationVariable);
+        animator.SetInteger("Mushroom_Jump", animationVariable = 1);
+        animator.SetInteger("Mushroom_Attacked", animationVariable = 2);
+    }
     void OnMouseDown()
     {
+        // 각 번호를 매기고 누르면 번호를 바꿔서 부르게
         // animationVariable을 수정
-        animator.SetInt("Mushroom_Idle", animationVariable);
-        animator.
+      
     }
 }
